3fb141ec5beedd93a8ffa0a9540836bf5da151a3
Kaio Mendes <kaiopablo17@gmail.com>
2023-08-10 17:02:30 -0300

    Landing Page of massages

---
+++ script.js
@@ -0,0 +1,16 @@
+// Scroll suave ao clicar nos links do menu
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        window.scrollTo({
+            top: target.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+
+
+
